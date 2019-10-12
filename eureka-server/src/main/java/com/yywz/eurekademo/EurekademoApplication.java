b0400a82996d108f998d2cd35d1982923bac86e9
yywzgh <yywzgh@gmail.com>
2019-10-12 11:27:12 +0800

    midify class name

--- eureka-server/src/main/java/com/yywz/eurekademo/EurekademoApplication.java
+++ eureka-server/src/main/java/com/yywz/eurekademo/EurekademoApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekademoApplication {
 
 	public static void main(String[] args) {
+		//
 		SpringApplication.run(EurekademoApplication.class, args);
 	}
 
