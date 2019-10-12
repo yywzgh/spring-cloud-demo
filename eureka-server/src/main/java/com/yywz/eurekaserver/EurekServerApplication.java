a41f74e5c59e01767f8ecef5cd1158d518382b5c
yywzgh <yywzgh@gmail.com>
2019-10-12 11:37:01 +0800

    change name

--- eureka-server/src/main/java/com/yywz/eurekaserver/EurekServerApplication.java
+++ eureka-server/src/main/java/com/yywz/eurekaserver/EurekServerApplication.java
@@ -1,4 +1,4 @@
-package com.yywz.eurekademo;
+package com.yywz.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,11 +6,11 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekademoApplication {
+public class EurekServerApplication {
 
 	public static void main(String[] args) {
 		//
-		SpringApplication.run(EurekademoApplication.class, args);
+		SpringApplication.run(EurekServerApplication.class, args);
 	}
 
 }
